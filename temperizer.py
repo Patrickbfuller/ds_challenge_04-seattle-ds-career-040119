3466fe84939eda31e36dfbb1c8c8bd604ebb8d7e
Patrick Fuller <patrickbfuller@gmail.com>
2019-04-04 09:24:16 -0700

    modify functions in import and apply to seperate filei

--- temperizer.py
+++ temperizer.py
@@ -7,6 +7,25 @@ def convert_f_to_c(temperature_f):
     return temperature_c
 
 
-def convert_c_to_f(temperature_c):
+def convert_c_to_f(temp_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    temp_f = (9/5) * temp_c + 32
+    return temp_f
+
+def convert_c_to_k(temp_c):
+    temp_k = temp_c + 273.15
+    return temp_k
+
+def convert_f_to_k(temp_f):
+    temp_c = convert_f_to_c(temp_f)
+    temp_k = convert_c_to_k(temp_c)
+    return temp_k
+
+def convert_k_to_c(temp_k):
+    temp_c = temp_k -273.15
+    return temp_c
+
+def convert_k_to_f(temp_k):
+    temp_c = temp_k -273.15
+    temp_f = convert_c_to_f(temp_c)
+    return temp_f
